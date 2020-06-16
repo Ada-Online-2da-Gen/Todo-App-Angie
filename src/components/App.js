510386835ca3c8de9e5a15cee44fd4b20f890227
Angie Turne <angie.turne@hotmail.com>
2020-06-16 16:12:14 -0300

    Actualizo input

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,40 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Button from 'components/Button/Button'
+import Container from 'components/Container/Container'
+import Input from 'components/Input/Input'
+import List from 'components/List/List'
+import ListItem from 'components/ListItem/ListItem'
 
 const App = () => {
-  return <></>
+  const [items, setItems] = useState([])
+  const [todo, setTodo] = useState('')
+
+  const addItemList = (event) => {
+    setTodo(event.target.value)
+    if (event.keyCode === 13 && event.target.value !== '') {
+      setItems([...items, todo])
+      event.target.value = ''
+    }
+  }
+
+  const addItemListButton = () => {
+    setItems([...items, todo])
+    setTodo('')
+  }
+
+  return (
+    <>
+      <Container>
+        <Input onChangeItem={addItemList} />
+        <Button onClick={addItemListButton}>Agregar</Button>
+        <List>
+          {items.map((item, index) => (
+            <ListItem key={index}>{item}</ListItem>
+          ))}
+        </List>
+      </Container>
+    </>
+  )
 }
 
 export default App
--- src/components/Input/Input.js
+++ src/components/Input/Input.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
-const Input = ({ ...props }) => {
-  return <input {...props} />
+const Input = ({ onChangeItem }) => {
+  return <input placeholder="Ingrese nuevo To Do" onKeyDown={onChangeItem} />
 }
 
 export default Input
