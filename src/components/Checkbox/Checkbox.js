3209baaf3c6b5a2060f8b171437581d1bd141298
Angie Turne <angie.turne@hotmail.com>
2020-06-20 17:27:41 -0300

    actualizo #4

---
+++ src/components/Checkbox/Checkbox.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const Checkbox = ({ handleCheckboxClick }) => {
+  return <input type="checkbox" onClick={handleCheckboxClick}></input>
+}
+
+export default Checkbox
--- src/components/Todo/Todo.js
+++ src/components/Todo/Todo.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import ListItem from 'components/ListItem/ListItem'
+import Checkbox from 'components/Checkbox/Checkbox'
 import { FaTrashAlt } from 'react-icons/fa'
 import style from './todo.module.scss'
 
@@ -7,13 +8,21 @@ const Todo = ({ todo, index, handleDeleteIconClick }) => {
   const [visible, setVisible] = useState(false)
   const handleIconDelete = () => setVisible(!visible)
 
+  const [statusCheckbox, setStatusCheckbox] = useState('pending')
+
+  const handleCheckboxClick = (event) => {
+    const status = event.target.checked ? 'completed' : 'pending'
+    setStatusCheckbox(status)
+  }
+
   return (
     <ListItem
-      className={style.listItem}
+      className={`${style[statusCheckbox]}`}
       onMouseEnter={handleIconDelete}
       onMouseLeave={handleIconDelete}
       key={index}
     >
+      <Checkbox handleCheckboxClick={handleCheckboxClick}></Checkbox>
       {todo}
       {visible && (
         <FaTrashAlt style={{ paddingLeft: '10px' }} onClick={() => handleDeleteIconClick(index)} />
